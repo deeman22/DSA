c9ca4c19f73c74cc4da39c6086b2a83fe9840863
Biswajit Das <78563798+deeman22@users.noreply.github.com>
2022-04-23 04:49:11 +0530

    Create LongestIncreasingSubsequence.cpp

---
+++ LongestIncreasingSubsequence.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+class Solution {
+public:
+    int res = 1;
+
+      // top down
+//     void fun(vector<int>& nums, int indx, int maxNow,int count, int n){
+        
+//         if(indx == n){
+//             if(count > res)
+//                 res = count;
+//             return;
+//         }
+        
+//         for(int i = indx; i<n; i++){
+//             if(maxNow < nums[i]){ 
+//                 fun(nums, i+1, nums[i], count+1, n);
+//             }
+//         }
+//         if(count > res) res = count;
+        
+//     }
+    
+    // bottom up
+    int helper (vector<int>& nums, int indx, int prevIndx, int n, vector<vector<int>> &hs){
+        
+        if(indx == n) return 0;
+        if(hs[indx][prevIndx+1] != -1) return hs[indx][prevIndx+1];
+        int len = 0 + helper(nums, indx+1, prevIndx, n, hs);
+        if(prevIndx == -1 || nums[indx] > nums[prevIndx]){
+            len = max(len,1+ helper(nums, indx+1, indx, n, hs));
+        }
+        return hs[indx][prevIndx+1] = len;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        vector<vector<int>> hs (n, vector<int>(n+1,-1));
+        
+        
+        //fun(nums, 0, INT_MIN, 0, nums.size());
+        return helper(nums, 0, -1, n, hs);
+        //return res;
+    }
+};
